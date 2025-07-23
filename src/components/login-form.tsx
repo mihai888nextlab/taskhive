0a904bf2bcc4681acbcdf2f6891744bd5b0e5b6f
ramiDev <147836937+mihai888nextlab@users.noreply.github.com>
2025-07-23 15:27:48 +0300

    fix: another small change for debugging

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -195,7 +195,7 @@ const GoogleAuthButton: React.FC<GoogleAuthButtonProps> = ({
         onLoginFailure(errorResponse);
       }
     },
-    redirect_uri: `${window.location.origin}/`, // Ensure this matches your backend endpoint
+    redirect_uri: `https://www.taskhive.tech/`, // Ensure this matches your backend endpoint
     // Crucial: Use 'auth-code' flow for backend verification
     flow: "auth-code",
   });
